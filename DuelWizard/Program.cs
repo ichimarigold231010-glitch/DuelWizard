68ba75cab871b59e4fc53cc7699a204104ca26f3
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-02-04 22:21:07 +0700

    menambahkan energi

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -1,8 +1,31 @@
-﻿public class Wizard
+﻿
+public class Wizard
 {
     public string nama;
-    public int nyawa;
+    public int energi;
     public int damage;
 
-    public void 
+    public Wizard(string nama, int damage)
+    {
+        nama = nama;
+        energi = 100;
+        damage = damage;
+    }
+
+    public void Attack(Wizard enemy)
+    {
+        enemy.energi -= damage;
+
+        if (enemy.energi < 0)
+            enemy.energi = 0;
+
+        Console.WriteLine($"{nama} menyerang {enemy.energi}!");
+        Console.WriteLine($"Energi {enemy.nama} tersisa: {enemy.energi}");
+    }
+    public void showStaats()
+    {
+        Console.WriteLine($"Nama wizard: {nama}");
+        Console.WriteLine($"Eneri: {energi}");
+    }
 }
+
