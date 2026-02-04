0d70a4d68d574b4708518718dc1bcc8ef57b7946
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-02-04 22:26:42 +0700

    menambahkan wizard dll

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -22,10 +22,34 @@ public class Wizard
         Console.WriteLine($"{nama} menyerang {enemy.energi}!");
         Console.WriteLine($"Energi {enemy.nama} tersisa: {enemy.energi}");
     }
-    public void showStaats()
+    public void showStats()
     {
         Console.WriteLine($"Nama wizard: {nama}");
         Console.WriteLine($"Eneri: {energi}");
     }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wizard wizard1 = new Wizard("Mimi", 20);
+            Wizard wizard2 = new Wizard("Peri", 15);
+
+
+            wizard1.showStats();
+            wizard2.showStats();
+
+            wizard1.Attack(wizard2);
+            wizard2.Attack(wizard1);
+            wizard1.Attack(wizard2);
+
+            Console.WriteLine("=== Statistik Akhir ===");
+            wizard1.showStats();
+            wizard2.showStats();
+
+            Console.ReadLine();
+        }
+
+    }
 }
+
 
