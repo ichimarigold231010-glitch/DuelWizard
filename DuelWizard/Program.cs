f9dffb0c988417c3303969c3569de918f5fca603
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-02-04 22:11:15 +0700

    menambahkan awal biasa

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Wizard
+{
+    public string nama;
+    public int nyawa;
+    public int damage;
+
+    public void 
+}
