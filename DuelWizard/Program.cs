a05df617c6a27e2a246f22a8d8d3f9a5b06e97b8
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-02-06 15:07:04 +0700

    menambhakan kode bu bias

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -1,55 +1,106 @@
 ﻿
+//public class Wizard
+//{
+//    public string nama;
+//    public int energi;
+//    public int damage;
+
+//    public Wizard(string nama, int damage)
+//    {
+//        nama = nama;
+//        energi = 100;
+//        damage = damage;
+//    }
+
+//    public void Attack(Wizard enemy)
+//    {
+//        enemy.energi -= damage;
+
+//        if (enemy.energi < 0)
+//            enemy.energi = 0;
+
+//        Console.WriteLine($"{nama} menyerang {enemy.energi}!");
+//        Console.WriteLine($"Energi {enemy.nama} tersisa: {enemy.energi}");
+//    }
+//    public void showStats()
+//    {
+//        Console.WriteLine($"Nama wizard: {nama}");
+//        Console.WriteLine($"Eneri: {energi}");
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Wizard wizard1 = new Wizard("Mimi", 20);
+//            Wizard wizard2 = new Wizard("Peri", 15);
+
+
+//            wizard1.showStats();
+//            wizard2.showStats();
+
+//            wizard1.Attack(wizard2);
+//            wizard2.Attack(wizard1);
+//            wizard1.Attack(wizard2);
+
+//            Console.WriteLine("=== Statistik Akhir ===");
+//            wizard1.showStats();
+//            wizard2.showStats();
+
+//            Console.ReadLine();
+//        }
+
+//    }
+//}
+
+
+//BU BIAS CODE
+
+Wizard wizardA = new Wizard("Mimi peri", 5);
+Wizard wizardB = new Wizard("Dory", 10);
+
+Console.WriteLine("Permainan dimulai...\n");
+Console.WriteLine("Statistik Awal");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA);
+wizardA.Attack(wizardB);
+
+Console.WriteLine("Permainan Selesai....");
+Console.WriteLine("Statistik Akhir");
+wizardA.ShowStats();
+wizardB.ShowStats();
+
+
 public class Wizard
 {
-    public string nama;
-    public int energi;
-    public int damage;
+    public string Name;
+    public int Energy;
+    public int Damage;
 
-    public Wizard(string nama, int damage)
+//constructor
+
+    public Wizard(string name, int damage)
     {
-        nama = nama;
-        energi = 100;
-        damage = damage;
+        Name = name;
+        Energy = 100;
+        Damage = damage;
+    }
+    //deklarasi method
+    public void ShowStats()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Energy: {Energy}\n");
     }
 
-    public void Attack(Wizard enemy)
+    public void Attack(Wizard enemyObj)
     {
-        enemy.energi -= damage;
-
-        if (enemy.energi < 0)
-            enemy.energi = 0;
-
-        Console.WriteLine($"{nama} menyerang {enemy.energi}!");
-        Console.WriteLine($"Energi {enemy.nama} tersisa: {enemy.energi}");
+        enemyObj.Energy -= Damage;
+        Console.WriteLine($"{Name} menyerang {enemyObj.Name}");
+        Console.WriteLine($"Sisa energi {enemyObj.Name}: {enemyObj.Energy}");
     }
-    public void showStats()
-    {
-        Console.WriteLine($"Nama wizard: {nama}");
-        Console.WriteLine($"Eneri: {energi}");
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Wizard wizard1 = new Wizard("Mimi", 20);
-            Wizard wizard2 = new Wizard("Peri", 15);
 
-
-            wizard1.showStats();
-            wizard2.showStats();
-
-            wizard1.Attack(wizard2);
-            wizard2.Attack(wizard1);
-            wizard1.Attack(wizard2);
-
-            Console.WriteLine("=== Statistik Akhir ===");
-            wizard1.showStats();
-            wizard2.showStats();
-
-            Console.ReadLine();
-        }
-
-    }
 }
 
 
